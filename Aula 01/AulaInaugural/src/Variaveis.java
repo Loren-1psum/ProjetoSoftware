e541dc0580c248d5640ec09be583acc04cc99999
Loren-1psum <Caioloren28@gmail.com>
2026-02-10 10:16:15 -0300

    Java Lesson 01

---
+++ Aula 01/AulaInaugural/src/Variaveis.java
@@ -0,0 +1,15 @@
+public class Variaveis {
+    public static void main(String[] args) {
+
+        int idade = 35;
+
+        double altura = 1.80;
+        double peso = 85.90;
+
+        char genero = 'F'; // Caracteres - ''
+        String nome = "Fulano"; // String - ""
+
+        System.out.println("Nome: " + nome + "Idade: " + idade + "Altura: " + altura + "Peso: " + peso);
+
+    }
+}
