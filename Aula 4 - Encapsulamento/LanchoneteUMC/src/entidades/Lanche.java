6723ff4beaff28c0b89894af81169482a1d14ede
Sout-Ryan-Two <soutogase2@gmail.com>
2026-02-26 10:57:30 -0300

    .

--- Aula 4 - Encapsulamento/LanchoneteUMC/src/entidades/Lanche.java
+++ Aula 4 - Encapsulamento/LanchoneteUMC/src/entidades/Lanche.java
@@ -7,14 +7,40 @@ public class Lanche {
     private int estoque;
     private boolean disponivel;
 
+
+
     //Construtor
-
-
-    public Lanche(String nome, String descricao, double valor, int estoque, boolean disponivel) {
+    public Lanche(String nome, String descricao, double valor, int estoque) {
         this.nome = nome;
         this.descricao = descricao;
         this.valor = valor;
         this.estoque = estoque;
-        this.disponivel = disponivel;
+        this.disponivel = estoque > 0;
+    }
+
+    //Getter
+
+
+
+    //Setter
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome +
+        "\nDescricao:'" + descricao +
+        "\nValor: R$" + valor +
+        "\nEstoque:" + estoque +
+        "\nDisponivel:" + disponivel;
     }
 }
