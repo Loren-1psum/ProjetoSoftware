f755dcf83a9562a31305e4c9d3c9bb69f4071e95
Loren-1psum <caioloren28@gmail.com>
2026-05-07 10:34:37 -0300

    .

--- SpringBoot/lojaRoupas/src/main/java/com/dgmodels/lojaRoupas/controller/ControllerProduto.java
+++ SpringBoot/lojaRoupas/src/main/java/com/dgmodels/lojaRoupas/controller/ControllerProduto.java
@@ -1,19 +1,29 @@
 package com.dgmodels.lojaRoupas.controller;
 
 import com.dgmodels.lojaRoupas.model.DadosCadastroRoupa;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.dgmodels.lojaRoupas.model.Roupa;
+import com.dgmodels.lojaRoupas.repository.RoupaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/roupa")
-
 public class ControllerProduto {
+
+    @Autowired
+    private RoupaRepository roupaRepository;
 
     @PostMapping
     public void cadastrarProduto(@RequestBody DadosCadastroRoupa dados){
-        System.out.println(dados);
+        roupaRepository.save(new Roupa (dados));
     }
-    
+
+
+    @GetMapping
+    public List<Roupa> listarProdutos(){
+        return roupaRepository.findAll();
+    }
+
 }
---
+++ SpringBoot/lojaRoupas/src/main/java/com/dgmodels/lojaRoupas/repository/RoupaRepository.java
@@ -0,0 +1,8 @@
+package com.dgmodels.lojaRoupas.repository;
+
+import com.dgmodels.lojaRoupas.model.Roupa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoupaRepository extends JpaRepository<Roupa, Long> {
+
+}
